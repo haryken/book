638ab512ca5d662d896ccc0c7ad44bce479dbb79
haryken <64195026+haryken@users.noreply.github.com>
2022-07-14 00:10:49 +0700

    a

--- config/app.php
+++ config/app.php
@@ -14,7 +14,6 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'force_https' => env('FORCE_HTTPS', false),
 
     /*
     |--------------------------------------------------------------------------
